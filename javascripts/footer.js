0b4ed7f7a355de74c105d29a60c3f9d68c6045b2
Pranav Parikh <pranav.r.parikh@gmail.com>
2013-12-05 21:53:27 -0800

    Adding footer

---
+++ javascripts/footer.js
@@ -0,0 +1,8 @@
+document.write("<!-- FOOTER  -->");
+document.write("<div id=\"footer_wrap\" class=\"outer\">");
+document.write("    <footer class=\"inner\">");
+document.write("        <p class=\"copyright\">Arrow maintained by <a href=\"https:\/\/github.com\/yahoo\">Yahoo<\/a><\/p>");
+document.write("        <p>Published with <a href=\"http:\/\/pages.github.com\">GitHub Pages<\/a><\/p>");
+document.write("    <\/footer>");
+document.write("<\/div>");
+document.write("");
