9f8be04706f7b4956b16187386e78a791ade8d6e
Pranav Parikh <pranav.r.parikh@gmail.com>
2013-12-05 21:10:22 -0800

    Fixing header and menu

---
+++ javascripts/header.js
@@ -0,0 +1,14 @@
+document.write("<div id=\"header_wrap\" class=\"outer\">");
+document.write("    <header class=\"inner\">");
+document.write("        <a id=\"forkme_banner\" href=\"https:\/\/github.com\/yahoo\/arrow\">View on GitHub<\/a>");
+document.write("");
+document.write("        <h1 id=\"project_title\">Arrow<\/h1>");
+document.write("        <h2 id=\"project_tagline\">FE Test framework designed to promote TDD<\/h2>");
+document.write("");
+document.write("        <section id=\"downloads\">");
+document.write("            <a class=\"zip_download_link\" href=\"https:\/\/github.com\/yahoo\/arrow\/zipball\/master\">Download this project as a .zip file<\/a>");
+document.write("            <a class=\"tar_download_link\" href=\"https:\/\/github.com\/yahoo\/arrow\/tarball\/master\">Download this project as a tar.gz file<\/a>");
+document.write("        <\/section>");
+document.write("    <\/header>");
+document.write("<\/div>");
+document.write("");
